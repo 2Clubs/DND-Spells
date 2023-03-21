c81f8a03f23e8099e57ccc38a30752e4b0dcbf97
2clubs <bennettdan85@gmail.com>
2023-03-21 19:37:41 -0400

    fixes description bug

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-let spellData, userInput;
+let spellData, userInput
 
 const $spellNameHeader = $(`<h3>Name</h3>`);
 const $spellName = $(`<p id="name"></p>`);
 const $spellDescHeader = $(`<p>Description</p>`);
+const $spellDesc = $(`<p></p>`)
 const $spellLevelHeader = $(`<p>Level</p>`);
 const $spellLevel = $(`<p id="level"></p>`);
 const $main = $(`main`);
@@ -20,7 +21,7 @@ function handleGetData(event) {
       spellData = data;
       render();
       $input.val("");
-      console.log(spellData);
+      // console.log(spellData);
     },
     (error) => {
       console.log("bad request: ", error);
@@ -34,9 +35,12 @@ function render() {
   $spellName.text(spellData.name);
   // $spellDesc.text(spellData.desc)0
   $main.append($spellDescHeader);
+  $spellDesc.empty()
   for(let i in spellData.desc){
-    $main.append($(`<p></p>`).text(spellData.desc[i]));
+    $spellDesc.append($(`<p></p>`).text(spellData.desc[i]));
+    console.log($spellDesc)
   }
+  $main.append($spellDesc)
   $main.append($spellLevelHeader);
   $main.append($spellLevel);
   $spellLevel.text(spellData.level);
