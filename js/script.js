90bac91606fcb098f4f8bd354fd486b26a41112f
2clubs <bennettdan85@gmail.com>
2023-03-21 13:16:23 -0400

    adds initial spell list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let spellData, userInput;
 const $spellNameHeader = $(`<h3>Name</h3>`);
 const $spellName = $(`<p id="name"></p>`);
 const $spellDescHeader = $(`<p>Description</p>`);
-const $spellDesc = $(`<p id="description"></p>`);
+const $spellDescOne = $(`<p id="description-one"></p>`);
+const $spellDescTwo = $(`<p id="description-two"></p>`);
 const $spellLevelHeader = $(`<p>Level</p>`);
 const $spellLevel = $(`<p id="level"></p>`);
 const $main = $(`main`);
@@ -13,14 +14,15 @@ $("form").on("submit", handleGetData);
 
 function handleGetData(event) {
   event.preventDefault();
-  userInput = $input.val();
+  userInput = $input.val().replace(/\s+/g, "-").toLowerCase(); //https://stackoverflow.com/questions/1983648/replace-spaces-with-dashes-and-make-all-letters-lower-case
   $.ajax({
     url: `https://www.dnd5eapi.co/api/spells/${userInput}`,
   }).then(
     (data) => {
       spellData = data;
       render();
-        $input.val('');
+      $input.val("");
+      console.log(spellData);
     },
     (error) => {
       console.log("bad request: ", error);
@@ -34,8 +36,10 @@ function render() {
   $spellName.text(spellData.name);
   // $spellDesc.text(spellData.desc)
   $main.append($spellDescHeader);
-  $main.append($spellDesc);
-  $spellDesc.text(spellData.desc);
+  $main.append($spellDescOne);
+  $main.append($spellDescTwo);
+  $spellDescOne.text(spellData.desc[0]);
+  $spellDescTwo.text(spellData.desc[1]);
   // $spellLevel.text(spellData.level)
   $main.append($spellLevelHeader);
   $main.append($spellLevel);
