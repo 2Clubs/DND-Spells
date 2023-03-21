a2b0f3eb2a5e6969f3837cb09cad474de4decfb2
2clubs <bennettdan85@gmail.com>
2023-03-21 16:44:40 -0400

    adds dynamic descriptions

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ let spellData, userInput;
 const $spellNameHeader = $(`<h3>Name</h3>`);
 const $spellName = $(`<p id="name"></p>`);
 const $spellDescHeader = $(`<p>Description</p>`);
-const $spellDescOne = $(`<p id="description-one"></p>`);
-const $spellDescTwo = $(`<p id="description-two"></p>`);
 const $spellLevelHeader = $(`<p>Level</p>`);
 const $spellLevel = $(`<p id="level"></p>`);
 const $main = $(`main`);
@@ -34,13 +32,11 @@ function render() {
   $main.append($spellNameHeader);
   $main.append($spellName);
   $spellName.text(spellData.name);
-  // $spellDesc.text(spellData.desc)
+  // $spellDesc.text(spellData.desc)0
   $main.append($spellDescHeader);
-  $main.append($spellDescOne);
-  $main.append($spellDescTwo);
-  $spellDescOne.text(spellData.desc[0]);
-  $spellDescTwo.text(spellData.desc[1]);
-  // $spellLevel.text(spellData.level)
+  for(let i in spellData.desc){
+    $main.append($(`<p></p>`).text(spellData.desc[i]));
+  }
   $main.append($spellLevelHeader);
   $main.append($spellLevel);
   $spellLevel.text(spellData.level);
