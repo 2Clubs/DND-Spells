5767816d51b9714c2eb1c235a6d47b9bc14316d3
2clubs <bennettdan85@gmail.com>
2023-03-21 20:02:48 -0400

    adds additional properties

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-let spellData, userInput
+let spellData, userInput;
 
 const $spellNameHeader = $(`<h3>Name</h3>`);
 const $spellName = $(`<p id="name"></p>`);
 const $spellDescHeader = $(`<p>Description</p>`);
-const $spellDesc = $(`<p></p>`)
+const $spellDesc = $(`<p id="description"></p>`);
 const $spellLevelHeader = $(`<p>Level</p>`);
 const $spellLevel = $(`<p id="level"></p>`);
+const $spellRangeHeader = $(`<p>Range</p>`);
+const $spellRange = $(`<p id="range"></p>`);
+const $spellSchoolHeader = $(`<p>School</p>`)
+const $spellSchool = $(`<p id="school"></p>`)
+const $spellMaterialsHeader = $(`<p>Materials</p>`)
+const $spellMaterials = $(`<p id="materials"></p>`)
 const $main = $(`main`);
 const $input = $(`input[type="text"]`);
 
@@ -33,15 +39,24 @@ function render() {
   $main.append($spellNameHeader);
   $main.append($spellName);
   $spellName.text(spellData.name);
-  // $spellDesc.text(spellData.desc)0
-  $main.append($spellDescHeader);
-  $spellDesc.empty()
-  for(let i in spellData.desc){
-    $spellDesc.append($(`<p></p>`).text(spellData.desc[i]));
-    console.log($spellDesc)
-  }
-  $main.append($spellDesc)
   $main.append($spellLevelHeader);
   $main.append($spellLevel);
   $spellLevel.text(spellData.level);
+  $main.append($spellSchoolHeader)
+  $main.append($spellSchool)
+  $spellSchool.text(spellData.school.name)
+  $main.append($spellRangeHeader);
+  $main.append($spellRange);
+  $spellRange.text(spellData.range);
+  $main.append($spellMaterialsHeader)
+  $main.append($spellMaterials)
+  $spellMaterials.text(spellData.material)
+  // $spellDesc.text(spellData.desc)0
+  $main.append($spellDescHeader);
+  $spellDesc.empty();
+  for (let i in spellData.desc) {
+    $spellDesc.append($(`<p></p>`).text(spellData.desc[i]));
+    console.log($spellDesc);
+  }
+  $main.append($spellDesc);
 }
